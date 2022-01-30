2208cccca6330a63398d68d6c5a87d9579f36c28
Nauman <nauman001@yahoo.com>
2022-01-30 16:44:33 +0100

    wip: projects and its sub pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,18 +7,12 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `notes`,
-        path: `${__dirname}/src/notes/`,
-      },
-    },
+    "gatsby-transformer-remark",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
-        path: `${__dirname}/src/pages/projects/`,
+        path: `${__dirname}/src/projects/`,
       },
     },
   ],
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,44 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
 
-export default function Projects() {
+export default function Projects({ data }) {
+  console.log(data)
+  const projects = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h1>Projects</h1>
         <h2>Projects & websites I have created</h2>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={"projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
